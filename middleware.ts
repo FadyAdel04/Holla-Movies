6927bd7682d392643353ecca832fd9e183e3b177
fady <fastfady2019@gmail.com>
2024-09-25 01:23:46 +0200

    Describe your changes

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from "next/server";
-import { clerkMiddleware } from "@clerk/nextjs/server"; // Ensure this import is correct
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Use Clerk middleware to check authentication
-export default clerkMiddleware({
-  publicRoutes: ["/", "/profile"], // Allow public access to specific pages
-});
+export default clerkMiddleware();
 
-// Matcher configuration
 export const config = {
-  matcher: ["/((?!_next/image|_next/static|favicon.ico).*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
